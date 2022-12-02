1e2cade8e5540fa5c1dfbb390afd9b85acac0df8
lordW1lly <sebastianengelstajn@gmail.com>
2022-12-02 18:27:10 -0300

    agregando vistas

--- SistemaWebEmpleado/Controllers/EmpleadoController.cs
+++ SistemaWebEmpleado/Controllers/EmpleadoController.cs
@@ -3,6 +3,7 @@ using SistemaWebEmpleado.Data;
 using System.Linq;
 using System.Collections.Generic;
 using SistemaWebEmpleado.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace SistemaWebEmpleado.Controllers
 {
@@ -49,6 +50,56 @@ namespace SistemaWebEmpleado.Controllers
                                     select p).ToList();
             return View("Index", lista);
         }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+
+            Empleado empleado = _context.empleados.Find(id);
+
+            return View("Edit", empleado);
+
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Empleado empleado)
+        {
+
+            if (ModelState.IsValid)
+            {
+
+                _context.Entry(empleado).State = EntityState.Modified;
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(empleado);
+
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            Empleado empleado = _context.empleados.Find(id);
+
+            return View("Delete", empleado);
+        }
+
+        //GET: /Turno/Delete/id
+        [HttpPost]
+        [ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            Empleado empleado = _context.empleados.Find(id);
+            if (empleado != null)
+            {
+                _context.empleados.Remove(empleado);
+                _context.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+
     } 
 
 }
